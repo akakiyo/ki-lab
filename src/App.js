de1dc673b4d556be6adb8f6d0ac9d5dfd6e21704
kiyo <kiyo.a328@gmail.com>
2022-02-21 22:59:06 +0900

    無駄なコードの削除

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Chart from "react-apexcharts";
 
 function App() {
+  const [options, setOptions] = useState({
+    charts: {
+      id: "basic-bar",
+    },
+  });
+  const [xaxis, setXaxis] = useState({
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+  });
+  const [series, setSeries] = useState([
+    { name: "series-1", data: [30, 40, 45, 50, 49, 60, 70, 91] },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="row">
+        <div className="mixed-chart">
+          <Chart options={options} series={series} type="line" width="500" />
+        </div>
+      </div>
     </div>
   );
 }
