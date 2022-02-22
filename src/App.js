92ad2afdc6ec7c5ecb3c3eef2c794909129da2b3
kiyo <kiyo.a328@gmail.com>
2022-02-22 19:50:26 +0900

    csvファイルのアップロード機能の追加

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Chart from "react-apexcharts";
+import { useDropzone } from "react-dropzone";
+import { usePapaParse } from "react-papaparse";
 
 function App() {
+  const { readString } = usePapaParse();
   const [options, setOptions] = useState({
     charts: {
       id: "basic-bar",
@@ -10,17 +13,57 @@ function App() {
   const [xaxis, setXaxis] = useState({
     categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
   });
-  const [series, setSeries] = useState([
-    { name: "series-1", data: [30, 40, 45, 50, 49, 60, 70, 91] },
-  ]);
+  const [EEGSignals, setEEGSignals] = useState([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        const binaryStr = reader.result;
+        const data = [];
+        readString(binaryStr, {
+          worker: true,
+          complete: (results) => {
+            results.data.map((row) => {
+              data.push({
+                dataNum: row[0],
+                subjectName: row[1],
+                label: row[2],
+                electRode: row[3],
+                data: row.slice(4, 304).map(Number),
+              });
+            });
+            setEEGSignals(data);
+          },
+        });
+      };
+      reader.readAsText(file);
+    });
+  }, []);
+  console.log(EEGSignals);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
-    <div className="app">
-      <div className="row">
-        <div className="mixed-chart">
-          <Chart options={options} series={series} type="line" width="500" />
+    <>
+      <div className="app">
+        <div className="row">
+          <div className="mixed-chart">
+            <Chart options={options} type="line" width="500" />
+          </div>
         </div>
       </div>
-    </div>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+    </>
   );
 }
 
